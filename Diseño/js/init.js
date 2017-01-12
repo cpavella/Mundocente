31219a1462fb2036a0fb835f8290f59925c3a020
Diego Sosa <diego.sosa0704@gmail.com>
2017-01-12 18:48:01 -0500

    Dieño inicial de LandingPage

---
+++ Diseño/js/init.js
@@ -0,0 +1,17 @@
+/**
+ * Created by diego on 10/01/2017.
+ */
+
+$(document).ready(function () {
+    // fix menu when passed
+
+    $('.navbar').visibility({
+        once: false,
+        onBottomPassed: function () {
+            $('.navbar-fixed').transition('fade left');
+        },
+        onBottomPassedReverse: function () {
+            $('.navbar-fixed').transition('fade');
+        }
+    });
+});
